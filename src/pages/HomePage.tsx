79f36dfc016f49841a422e9c331e125ad22d511f
aliakbarzohour <aliakbar.zohour@gmail.com>
2024-07-12 12:50:23 +0330

    feat: Login Page

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,8 +1,11 @@
+import { Link } from "react-router-dom"
+
 const HomePage = () => {
     return (
-        <div>
-            Home Page
-        </div>
+        <section className="w-full h-screen flex justify-center items-center flex-col bg-gray-300">
+            برای ثبت نام وارد شوید
+            <Link to={'/login'} className="py-2 px-12 mt-5 rounded-xl bg-black text-white">ورود</Link>
+        </section>
     )
 }
 
--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -1,6 +1,12 @@
+import { Link } from "react-router-dom"
+
 const NotFoundPage = () => {
     return (
-        <div>NotFoundPage</div>
+        <div className="w-full h-screen flex flex-col justify-center items-center bg-gray-500">
+            <h1 className="text-white font-bold text-9xl">404</h1>
+            <span className="text-white font-thin text-2xl">Page Not Found</span>
+            <Link to={'/'} className="rounded-xl bg-black text-white mt-12 px-8 py-3">back to home</Link>
+        </div>
     )
 }
 
